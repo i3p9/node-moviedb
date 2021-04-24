2f6c6d8fd51eb731da78a9845f7a911346dd7346
i3p9 <i3p9@users.noreply.github.com>
2021-04-25 01:19:25 +0600

    author route, showall/search options

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
+const authorRouter =require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views') //append views folder
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ limit: '10mb', extended: 'false'}))
 
 //Start db
 mongoose.connect(process.env.DATABASE_URL,
@@ -23,6 +26,6 @@ db.once('open', error => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
-
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
